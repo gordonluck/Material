719b21bac57476f20efe873f3f9af498705f54a7
themenote <loobome@gmail.com>
2017-09-26 21:15:09 +0800

    导航

--- header-login.php
+++ header-login.php
@@ -11,4 +11,19 @@
 </head>
 
 <body class="<?php if ( is_page('login-in') ){echo "login-page"; }else {echo"signup-page";} ?>">
-	
+	<nav class="navbar navbar-info navbar-transparent navbar-absolute">
+    	<div class="container">
+        	<div class="navbar-header">
+        		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example">
+            		<span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+        		</button>
+        	</div>
+
+        	<div class="collpase navbar-collapse">
+        		<?php wp_nav_menu( array( 'walker' => new JWalker_Nav_Menu(),'theme_location' => 'login-menu','menu_id'=>'main-menu','menu_class'=>'nav navbar-nav navbar-right','container'=>'ul')); ?>
+        	</div>
+    	</div>
+    </nav>
